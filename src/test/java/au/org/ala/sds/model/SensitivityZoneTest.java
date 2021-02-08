36ce66180b8103d16a9d63bc21b86174c79349e7
pal155 <Doug.Palmer@csiro.au>
2021-02-09 10:41:13 +1100

    Fix test cases in travis

--- src/test/java/au/org/ala/sds/model/SensitivityZoneTest.java
+++ src/test/java/au/org/ala/sds/model/SensitivityZoneTest.java
@@ -14,12 +14,17 @@
  ***************************************************************************/
 package au.org.ala.sds.model;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
 public class SensitivityZoneTest {
+    @BeforeClass
+    public static void runOnce() throws Exception {
+        System.setProperty("sds.config.file", "/sds-test.properties");
+    }
 
     @Test
     public void isInAustralia() {
